64818a920235a929d1a3dbb547afdaeabeae9a91
hackiku <ivan@pipewriter.io>
2023-12-11 07:19:13 +0100

    airfoil table v1 with latex headers and 3 fake data.py points

--- pages/2_2_airfoil.py
+++ pages/2_2_airfoil.py
@@ -1,16 +1,32 @@
 # 2_2._airfoil.py
 
 import streamlit as st
-from data import Variable
+from data import Variable, airfoils
 from utils import spacer, variables_two_columns
 
 c_z_krst = Variable("Cruise Lift Coefficient", 0.247, r"C_{z_{krst}}", "m/s")
-c_z_max = Variable("Max Lift Coefficient", 1.46, r"C_{z_{max}}", "m/s")  # Assuming max lift coefficient is from NACA 65-212
-v_krst = Variable("Cruising Speed", 224.37, r"v_{krst}", "m/s")
 
+# Streamlit app code
 def main():
-    st.title("2. Airfoil tables")
+    st.title("Airfoil Data")
+    st.write("Prioritet parametara koje uzimamo u obzir pri izboru aeroprofila može varirati od jedne do druge kategorije letelica, pa se zato univerzalna metodologija teško može propisati. Pristup u izboru koji će biti prikazan u nastavku ima za cilj da ukaže na najbitnije parametre koji se uzimaju u obzir. Na osnovu relativne debljine aeroprofila i uslova da je cZopt ≈ cZkrst bira se po pet aeroprofila za koren i za kraj krila, a zatim se gleda koji su aeroprofili najpogodniji.")
 
+    # Airfoil selection (for future filtering implementation)
+    selected_airfoil = st.selectbox("Select an Airfoil", list(airfoils.keys()))
+    airfoil_data = airfoils[selected_airfoil]
+
+    # Display airfoil data for the selected airfoil
+    st.markdown("### Airfoil Data")
+    st.markdown(display_airfoil_table(airfoil_data), unsafe_allow_html=True)
+
+def display_airfoil_table(airfoil_data):
+    # Start the table and hide the headers with an HTML comment
+    table = "<!--- | " + " | ".join(airfoil_data.keys()) + " | --->\n"
+    table += "| " + " | ".join([f'<span title="{var.name}">${var.latex}$</span>' for var in airfoil_data.values()]) + " |\n"
+    table += "|---" * len(airfoil_data) + "|\n"
+    table += "| " + " | ".join([str(var.value) for var in airfoil_data.values()]) + " |\n"
+
+    return table
 
 if __name__ == "__main__":
     main()
