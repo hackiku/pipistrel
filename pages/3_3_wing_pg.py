9b9546df6498281b4959df862751a515e5877e79
hackiku <ivan@pipewriter.io>
2023-12-11 01:19:59 +0100

    aspect ratio good ux calculator. on to abstract formulas latex

--- pages/3_3_wing_pg.py
+++ pages/3_3_wing_pg.py
@@ -3,15 +3,29 @@
 import streamlit as st
 from data import Variable
 from utils import spacer, variables_two_columns
+import math
 
 # Instantiate variables using the Variable class
-c_z_krst = Variable("Cruise Lift Coefficient", 0.200, r"C_{z_{krst}}", "m/s")
-c_z_max = Variable("Max Lift Coefficient", 0.247, r"C_{z_{max}}", "m/s")
+c_z_krst = Variable("Cruise Lift Coefficient", 0.247, r"C_{z_{krst}}", "m/s")
+c_z_max = Variable("Max Lift Coefficient", 1.46, r"C_{z_{max}}", "m/s")  # Assuming max lift coefficient is from NACA 65-212
 v_krst = Variable("Cruising Speed", 224.37, r"v_{krst}", "m/s")
 alpha_n = Variable("Angle of Attack", -1.3, r"\alpha_n", "degrees")
 lambda_wing = Variable("Wing Aspect Ratio", 3.888, r"\lambda")
 n = Variable("Wing Taper Ratio", 0.520, "n")
 rho = Variable("Air Density at Cruise Altitude", 0.736116, r"\rho", "kg/m^3")
+
+l_s = Variable("Chord Length", 3.028, "l_s", "m")
+l_0 = Variable("Root Chord Length", 1.576, "l_0", "m")
+
+# n = 1.576 / l_s  # Taper ratio (n)
+
+# NACA Airfoil characteristics
+naca_65_212_c_z_max = 1.46
+naca_64_209_c_z_max = 1.40
+alpha_n_65_212 = -1  # Angle of attack at zero lift for NACA 65-212
+alpha_n_64_209 = -1.3  # Angle of attack at zero lift for NACA 64-209
+a_0_65_212 = 0.110  # Lift curve slope for NACA 65-212
+correction_factor = 0.859  # Correction for the angle of attack
 
 def main():
     st.title("3. Wing Design")
@@ -20,13 +34,62 @@ def main():
     st.header("3.1 Wing Lift Features")
 
     # Display variables using the two-column layout
-    variables_two_columns(c_z_krst)
+    
     variables_two_columns(c_z_max)
-    variables_two_columns(v_krst)
     variables_two_columns(alpha_n)
-    variables_two_columns(lambda_wing)
+    
+    st.markdown("***")
+
+    # 3.1.1.
+    st.subheader("3.1.1. Max lift coefficient of wings")
+    st.write("Za proračun uzgonskih karakteristika krila i dobijanje podataka za formiranje krive uzgona je korišćen program Trapezno krilo - Glauert, a ulazni parametri su:")
+
+    # vkrst = 224.37 m/s = 807.73 km/h
+    # # alpha crit 
+    variables_two_columns(c_z_krst)
+    variables_two_columns(v_krst)
+    variables_two_columns(rho) # ρ
+    
+
+    # lambda      
+    st.markdown("***")
+    st.write("Calculate wing aspect ratio (λ):")
+
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        b = st.number_input('Wingspan b [m]', value=8.942, step=0.01)
+    with col2:
+        S = st.number_input('Wing area S [m²]', value=20.602, step=0.1)
+    with col3:
+        lambda_wing.value = b**2 / S 
+        lambda_calculated = st.number_input('Aspect Ratio λ', value=lambda_wing.value, step=0.001, format="%.3f")
+
+    st.latex("\\lambda = \\frac{b^2}{S} = \\frac{" + f"{b:.2f}^2" + "}{ " + f"{S:.2f}" + "} = " + f"{lambda_calculated:.3f}" + "")
+
+
+    
+    st.markdown("***")
+
+    st.write("Calculate taper ratio (n):")
+    
     variables_two_columns(n)
-    variables_two_columns(rho)
+    
+    col1, col2, col3 = st.columns(3)
+    
+    
+
+
+    with col1:
+        l_0_value = st.number_input('Root Chord Length l_0 [m]', value=1.576, step=0.01)
+    with col2:
+        l_s_value = st.number_input('Chord Length l_s [m]', value=3.028, step=0.01)
+    with col3:
+        n_calculated = l_0_value / l_s_value
+        st.write(f"n = {n_calculated:.3f}")
+        n.value = n_calculated  # Update the variable's value
+
+
+    
 
     st.markdown("***")
 
--- utils.py
+++ utils.py
@@ -13,5 +13,6 @@ def variables_two_columns(var):
         var.value = new_value  # Update the variable's value
     with col2:
         # Display the LaTeX representation along with the value and unit
-        st.markdown(" .")
-        st.markdown(f"$${var.latex} = {var.value} \ {var.unit}$$")
+        st.markdown("` `")
+        formatted_value = f"{var.value:.3f}"  # This formats the value to 3 decimal places
+        st.markdown(f"$${var.latex} = {formatted_value} \ {var.unit}$$")
