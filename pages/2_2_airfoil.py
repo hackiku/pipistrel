3b650f806daa1f74f826acbdf43a47fe7105d225
hackiku <ivan@pipewriter.io>
2023-12-11 07:39:18 +0100

    airfoil table formatted

--- pages/2_2_airfoil.py
+++ pages/2_2_airfoil.py
@@ -1,30 +1,31 @@
-# 2_2._airfoil.py
-
 import streamlit as st
 from data import Variable, airfoils
 from utils import spacer, variables_two_columns
 
 c_z_krst = Variable("Cruise Lift Coefficient", 0.247, r"C_{z_{krst}}", "m/s")
+Cz_op = Variable("Cruise Lift Coefficient", 0.247, r"C_{z_{krst}}", "m/s")
 
-# Streamlit app code
 def main():
     st.title("Airfoil Data")
     st.write("Prioritet parametara koje uzimamo u obzir pri izboru aeroprofila može varirati od jedne do druge kategorije letelica, pa se zato univerzalna metodologija teško može propisati. Pristup u izboru koji će biti prikazan u nastavku ima za cilj da ukaže na najbitnije parametre koji se uzimaju u obzir. Na osnovu relativne debljine aeroprofila i uslova da je cZopt ≈ cZkrst bira se po pet aeroprofila za koren i za kraj krila, a zatim se gleda koji su aeroprofili najpogodniji.")
 
-    # Airfoil selection (for future filtering implementation)
-    selected_airfoil = st.selectbox("Select an Airfoil", list(airfoils.keys()))
-    airfoil_data = airfoils[selected_airfoil]
-
-    # Display airfoil data for the selected airfoil
+    # Display airfoil data in a single table
     st.markdown("### Airfoil Data")
-    st.markdown(display_airfoil_table(airfoil_data), unsafe_allow_html=True)
+    st.markdown(display_airfoil_table(airfoils), unsafe_allow_html=True)
 
-def display_airfoil_table(airfoil_data):
-    # Start the table and hide the headers with an HTML comment
-    table = "<!--- | " + " | ".join(airfoil_data.keys()) + " | --->\n"
-    table += "| " + " | ".join([f'<span title="{var.name}">${var.latex}$</span>' for var in airfoil_data.values()]) + " |\n"
-    table += "|---" * len(airfoil_data) + "|\n"
-    table += "| " + " | ".join([str(var.value) for var in airfoil_data.values()]) + " |\n"
+def display_airfoil_table(airfoils_data):
+    # Create the header row, excluding the "Designation" key
+    header_keys = list(next(iter(airfoils_data.values())).keys())[1:]  # Skip the first key which is "Designation"
+    headers = ["Airfoil"] + header_keys
+    table = "<!--- | " + " | ".join(headers) + " | --->\n"
+    table += "| " + " | ".join(['<span style="font-size: 0.75em;">Airfoil</span>'] + [f'<span style="font-size: 0.75em;" title="{var.name}">${var.latex}$</span>' for var in next(iter(airfoils_data.values())).values()][1:]) + " |\n"
+    table += "|---" * len(headers) + "|\n"
+
+    # Add a row for each airfoil, excluding the "Designation" data
+    for name, data in airfoils_data.items():
+        row = "| " + f'<span style="font-size: 0.85em;">{name}</span>' + " | "
+        row += " | ".join([f'<span style="font-size: 0.8em;">{str(var.value)}</span>' for var in list(data.values())[1:]]) + " |\n"
+        table += row
 
     return table
 
