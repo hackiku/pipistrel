6222b8eb17b540b540b16816a05180ce2b8a59d7
hackiku <ivan@pipewriter.io>
2024-01-06 20:44:00 +0100

    quick blazing section 4

--- pages/4_4_drag_calculation.py
+++ pages/4_4_drag_calculation.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 from data import Variable
 from utils import spacer, emoji_header, variables_two_columns, variables_three_columns
 
+S = Variable("Wing Area", 30.00, "S", "S", "m²")
 S_20 = Variable("Aerodynamic reference area", 10.301, "S_20", "S_{20}", "m²")
 S_21 = Variable("Area of the wing exposed to airflow", 7.568, "S_21", "S_{21}", "m²")
 SWETKR = Variable("Wetted area of the wing", 30.877, "SWETKR", "S_{WETKR}", "m²")
@@ -85,17 +86,137 @@ def main():
     nu = 2.21e-5     # Kinematic viscosity in m^2/s
 
     calculate_aerodynamics(l0_value, lT_value, v_krst, nu)
+    
+    # graph
+    st.markdown("""<div style="background-color: black; opacity: 0.3; padding: 100px"></div>""", unsafe_allow_html=True)
+    spacer()
 
+    # ==================== cx wing ==================== #
+    
+    st.write("Iz gore priloženog dijagrama očitava se koeficijent otpora trenja Cf")
+    c_fkr = 0.0026
+    st.latex(rf"C_{{fKR}} = {c_fkr:.4f}")
+    st.markdown("***")
 
+    l0 = 1.574  # Root chord length in meters
+    dl0 = 0.09  # Relative thickness at the root
+    lT = 2.689  # Tip chord length in meters
+    dlT = 0.12  # Relative thickness at the tip
 
+    dl_effekKR = (l0 * dl0 + lT * dlT) / (l0 + lT)
+    
+    # Display the text and the equation
+    st.write("Efektivna relativna debljina se dobija osrednjavanjem relativnih debljina u korenu i na kraju krila jer su različite:")
+    st.latex(rf"(d/l)_{{effekKR}} = \frac{{l_0 \cdot (d/l)_0 + l_T \cdot (d/l)_T}}{{l_0 + l_T}} = \frac{{{l0:.3f} \cdot {dl0} + {lT:.3f} \cdot {dlT}}}{{{l0:.3f} + {lT:.3f}}} = {dl_effekKR:.3f}")
+    
+    S_WETKR = 30.877  # Wetted area of the wing in m^2
+    
+    spacer()
+    col1, col2 = st.columns(2)
+    with col1:
+        st.write("Faktor otpora oblika krila K očitava se sa dijagrama na sledećoj strani, a koji pokazuje zavisnost relativne debljine i ugla strele od faktora otpora oblika.")
+        dl = 0.109
+        phi = 10
+        st.latex(rf"(d/l)_{{effekKR}} = {dl:.3f}")
+        st.latex(rf"\phi = {phi:.3f}")
+        s = 20.602  # Reference area in square meters
+        k_kr = Variable("Wing shape drag", 1.21, "KKR", "K_{KR}", "")
+        variables_two_columns(k_kr)
+        c_x_min_krilo = (k_kr.value * c_fkr * S_WETKR) / s
+        st.write("Koeficijent minimalnog otpora krila")
+        st.latex(rf"C_{{X min krilo}} = \frac{{K_{{KR}} \cdot C_{{fKR}} \cdot S_{{WET_{{KR}}}}}}{{S}} = \frac{{{k_kr.value:.2f} \cdot {c_fkr:.4f} \cdot {S_WETKR:.3f}}}{{{s:.3f}}} = {c_x_min_krilo:.6f}")
 
+    with col2:
+        st.image('./assets/tmp_assets/koef_min_otpora.png', )
 
+    st.markdown("***")
+    
+    #======================================================#
+    #==================== (2) fuselage ====================#
+    #======================================================#
+
+    st.subheader("4.1.2. Trup (Fuselage)")
+
+    # Planform area of fuselage
+    st.write("Za određivanje okvašene površine trupa potrebno je izračunati površine u plan i bočnoj projekciji.")
+    S_tpl = sum([0.321, 1.432, 2.792, 2.588, 4.557, 1.326, 2.395, 1.784])
+    st.latex(r"S_{tpl} = \sum_{i=1}^{16} S_i = 0.321 + 1.432 + 2.792 + 2.588 + 4.557 + 1.326 + 2.395 + 1.784 = 17.193 m^2")
+
+    # Side area of fuselage
+    S_tb = sum([0.306, 1.255, 2.329, 2.931, 2.633, 4.426, 2.676, 1.051])
+    st.latex(r"S_{tb} = \sum_{i=9}^{16} S_i = 0.306 + 1.255 + 2.329 + 2.931 + 2.633 + 4.426 + 2.676 + 1.051 = 17.605 m^2")
+
+    # Wetted area of fuselage
+    st.write("Okvašena površina trupa")
+    S_WETT = (S_tpl + S_tb) * (2 - 0.4 * S_tpl / S_tb)
+    st.latex(r"S_{WETT} = (S_{tpl} + S_{tb}) \cdot \left(2 - 0.4 \cdot \frac{S_{tpl}}{S_{tb}}\right) = (17.193 + 17.605) \cdot \left(2 - 0.4 \cdot \frac{17.193}{17.605}\right) = 56 m^2")
+
+    # Length of the fuselage as read from the drawing
+    st.write("Proračunska dužina L očitana sa crteža je L_T=12.33m.")
+    st.latex(r"L_{T} = 12.33 m")
+
+    # Maximum cross-sectional area of fuselage in front projection
+    st.write("Površina maksimalnog poprečnog preseka trupa u čeonoj projekciji je S_max")
+    S_max_T = sum([0.917, 2.049, 0.268])
+    st.latex(r"S_{max_T} = \sum_{i=17}^{19} S_i = 0.917 + 2.049 + 0.268 = 3.234 m^2")
+
+    # Display a placeholder for the graph
+    st.markdown("""<div style="background-color: black; opacity: 0.3; padding: 100px"></div>""", unsafe_allow_html=True)
+
+    st.markdown("***")
+
+    col1, col2 = st.columns(2)
+    with col1:
+        st.write("Ekvivalentni prečnik maksimalnog poprečnog preseka trupa transformisanog u krug")
+        S_max_T = 3.234  # Maximum cross-sectional area in square meters
+        D_T = (4 * S_max_T / 3.14159) ** 0.5
+        st.latex(r"D_{T} = \sqrt{\frac{4 \cdot S_{max_{T}}}{\pi}} = \sqrt{\frac{4 \cdot 3.234}{\pi}} = 2.029m")
+
+        # Fineness ratio of fuselage
+        st.write("Vitkost trupa je tada")
+        L_T = 12.33  # Length of the fuselage
+        lambda_T = L_T / D_T
+        st.latex(r"\lambda_{T} = \frac{L_{T}}{D_{T}} = \frac{12.33}{2.029} = 6.08")
+
+        st.write("Faktor oblika trupa očitva se sa dijagrama")
+        K_T = 1.23
+        st.latex(r"K_{T} = 1.23")
+    with col2:
+        st.image('./assets/tmp_assets/koef_min_otpora.png', )
+
+    # Reynolds number for the fuselage
+    st.write("Rejnoldsov broj - potreban nam je za izračunavanje koeficijenta otpora trenja trupa")
+    Re_T = (v_krst * L_T) / nu
+    st.latex(r"Re = \frac{v_{krst} \cdot L_T}{\nu} = \frac{" + str(v_krst) + r" \cdot " + str(L_T) + r"}{" + str(nu) + r"} = " + str(round(Re_T, 2)))
+
+    # Drag coefficient from diagram
+    st.write("Iz prethodno datih dijagrama koeficijent otpora trenja trupa " + r"C_{fT} = " + str(CfKR.value))
+    st.latex(r"C_{fT} = " + str(CfKR.latex))
+
+    # Minimum drag coefficient of the fuselage
+    st.write("Koeficijent minimalnog otpora trupa")
+    # Given values
+    K_T_value = 1.23  # Fuselage shape factor
+    C_fT_value = 0.0026  # Coefficient of friction drag of the fuselage
+    S_WETT_value = 56  # Wetted area of the fuselage in square meters
+    S_value = 20.602  # Reference wing area in square meters
+
+    # Calculation of minimum drag coefficient of the fuselage
+    C_X_min_trup_value = (K_T_value * C_fT_value * S_WETT_value) / S_value
+
+    # Display the calculation in the app
+    st.write("Koeficijent minimalnog otpora trupa (Minimum drag coefficient of the fuselage):")
+    st.latex(rf"C_{{X min trup}} = \frac{{K_T \cdot C_{{fT}} \cdot S_{{WETT}}}}{{S}} = \frac{{{K_T_value} \cdot {C_fT_value} \cdot {S_WETT_value}}}{{{S_value}}} = {C_X_min_trup_value:.6f}")
+
+    #======================================================#
+    #============== (3) horizontal stabilizer ==============#
+    #======================================================#
+    st.subheader("4.1.3. Horizontalni rep (Horizontal Tail)")
+
+    
 
     #==================== spaceeee ====================#
     spacer('12em')
-    st.subheader("4.1.2. Trup (Fuselage)")
-
-    st.subheader("4.1.3. Horizontalni rep (Horizontal Tail)")
 
     st.subheader("4.1.4. Vertikalni rep (Vertical Tail)")
 
@@ -108,6 +229,7 @@ def main():
     st.subheader("4.2.1. Određivanje Osvaldovog faktora e i člana uz CZ2")
 
     st.subheader("4.2.2. Proračunska polara aviona (Aircraft Polar Calculation)")
+
 
 
 if __name__ == "__main__":
